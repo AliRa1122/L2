3b1f48ecc70b5557ae354eb253c5da33811a4221
AliRa1122 <Ali.raza@live.vu.edu.au>
2024-09-07 05:36:21 +0500

    Updated Details Activity

--- app/src/main/java/com/example/app/views/DetailsActivity.kt
+++ app/src/main/java/com/example/app/views/DetailsActivity.kt
@@ -1,23 +1,51 @@
 package com.example.app.views
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.app.R
+import com.example.app.databinding.ActivityDetailsBinding
+import com.example.app.models.Entity
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val entity = intent.getParcelableExtra<Entity>("ENTITY")
+        entity?.let { displayEntityDetails(it) }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayEntityDetails(entity: Entity) {
+        binding.apply {
+            speciesTextView.text = entity.species
+            scientificNameTextView.text = entity.scientificName
+            habitatTextView.text = "Habitat: ${entity.habitat}"
+            dietTextView.text = "Diet: ${entity.diet}"
+            conservationStatusTextView.text = "Conservation Status: ${entity.conservationStatus}"
+            averageLifespanTextView.text = "Average Lifespan: ${entity.averageLifespan} years"
+            descriptionTextView.text = entity.description
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
